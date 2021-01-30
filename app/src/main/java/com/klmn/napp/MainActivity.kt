258a81f092fd37c089657dba4fb87ca8ea866752
MichGur <michael@thegur.net>
2021-01-30 19:26:05 +0200

    Initial commit

---
+++ app/src/main/java/com/klmn/napp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.klmn.napp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+* Features:
+*   Retrofit / Room repository
+*   MVVM / MVI architecture
+* */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
